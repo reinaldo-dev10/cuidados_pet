80d463103affaa389d941c7cb2895709d6b1ba05
reinaldo-dev10 <reinaldocurso2010@hotmail.com>
2026-02-24 22:28:33 -0300

    updade2

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 const imagens = [
-  "./imgs/img1.jpg", //string representando o caminho da imagem
+  "./imgs/img1.jpg",
   "./imgs/img2.jpg",
   "./imgs/img3.png"
 ];
 
-//addEventListener é um método que adiciona um ouvinte de evento a um elemento HTML. 
-// Ele permite que você execute uma função específica quando um evento ocorre, como um clique, uma mudança de valor, etc. 
-// O primeiro argumento é o tipo de evento (por exemplo, "click"), e o segundo argumento é a função que será executada quando o evento ocorrer.
-
 document.getElementById("btn-galeria").addEventListener("click", () => {
-    const galeria = document.getElementById("imagens-galeria"); 
-    galeria.innerHTML = ""; // Limpa a galeria antes de adicionar novas imagens
-});
-
+    
+    const galeria = document.getElementById("imagens-galeria");
+    
+    galeria.innerHTML = ""; // limpa antes de adicionar
+    
     imagens.forEach((src) => {
-        const img = document.createElement("img");// Cria um elemento de imagem
-        img.src = src; // Define o atributo src da imagem para o caminho fornecido
-        img.alt = "Imagem da galeria"; // Define o atributo alt para acessibilidade
-        galeria.appendChild(img); // Adiciona a imagem à galeria  
+        const img = document.createElement("img");
+        img.src = src;
+        img.alt = "Imagem da galeria";
+        galeria.appendChild(img);
+    });
+
 });
-// () => {} é uma função anônima, ou seja, uma função sem nome.
-// Ela é frequentemente usada como um argumento para métodos como addEventListener, 
-// onde você deseja executar uma função específica quando um evento ocorre, 
-// mas não precisa reutilizar essa função em outro lugar do código.
