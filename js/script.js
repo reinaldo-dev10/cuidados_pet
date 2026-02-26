0e96855a5201bda16f07840847d44fa14bc3ac59
reinaldo-dev10 <reinaldocurso2010@hotmail.com>
2026-02-25 21:25:08 -0300

    js-pesquisa

--- js/script.js
+++ js/script.js
@@ -18,6 +18,37 @@ document.getElementById("btn-galeria").addEventListener("click", () => {
     });
 });
 
+//pesquisa de raças
+//para ciar listas usamos um array
+const listaRacas = [
+  { nome: "Labrador", caracteristica: "protetor" },
+  { nome: "Pastor Alemão", caracteristica: "corajoso" },
+  { nome: "Yorkshire", caracteristica: "caçador" },
+  { nome: "Poodle", caracteristica: "inteligente" },
+  { nome: "Bulldog", caracteristica: "calmo" },
+];
+
+document.getElementById("campo-filtro").addEventListener("input", (e) => {
+  console.log("digitando", e.target.value)
+  const valor = e.target.value.toLowerCase();
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+
+   listaRacas
+     .filter((raca) => raca.nome.toLowerCase().includes(valor))
+     .forEach((raca) => {
+       const li = document.createElement("li");
+       li.textContent = `${raca.nome} - ${raca.caracteristica}`;
+       resultado.appendChild(li);
+     });
+});
+
+
+
+
+
+
+
 // Captura o ano atual para o Copyright
 const ano = new Date().getFullYear();
 document.getElementById('ano-atual').textContent = ano;
